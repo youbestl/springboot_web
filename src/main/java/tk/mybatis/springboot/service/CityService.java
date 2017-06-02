5f0278e873040e4bf6d2573a81e2361766f79c3d
liangdong <youbestld@qq.com>
2017-06-02 15:35:21 +0800

    springboot_web

--- src/main/java/tk/mybatis/springboot/service/CityService.java
+++ src/main/java/tk/mybatis/springboot/service/CityService.java
@@ -46,22 +46,5 @@ public class CityService {
         if (city.getPage() != null && city.getRows() != null) {
             PageHelper.startPage(city.getPage(), city.getRows());
         }
-        return cityMapper.selectAll();
-    }
-
-    public City getById(Integer id) {
-        return cityMapper.selectByPrimaryKey(id);
-    }
-
-    public void deleteById(Integer id) {
-        cityMapper.deleteByPrimaryKey(id);
-    }
-
-    public void save(City country) {
-        if (country.getId() != null) {
-            cityMapper.updateByPrimaryKey(country);
-        } else {
-            cityMapper.insert(country);
-        }
     }
 }

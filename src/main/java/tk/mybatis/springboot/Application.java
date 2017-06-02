5ec307b32ce08f3c1162861f8d2f4a0a75edfa28
hanfei <1975772822@qq.com>
2017-06-02 17:12:35 +0800

    发发发

--- src/main/java/tk/mybatis/springboot/Application.java
+++ src/main/java/tk/mybatis/springboot/Application.java
@@ -24,6 +24,8 @@ public class Application extends WebMvcConfigurerAdapter {
 
     @RequestMapping("/")
     String home() {
+
+        String string = "ddd";
         return "redirect:countries";
     }
 }

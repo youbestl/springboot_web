b6815ce50d72f4371328508f8a53c5b02184edf3
liangdong <youbestld@qq.com>
2017-06-02 17:04:04 +0800

    springboot_web

---
+++ src/main/java/tk/mybatis/springboot/Test.java
@@ -0,0 +1,14 @@
+package tk.mybatis.springboot;
+
+/**
+ * Created by JARVIS on 2017/6/2.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1aaadfasdfas");
+        System.out.println("1aaadfasdfas");
+        System.out.println("1aaadfasdfas");
+        System.out.println("1aaadfasdfas");
+        System.out.println("1aaadfasdfas");
+    }
+}

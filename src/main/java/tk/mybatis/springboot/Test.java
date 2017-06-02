71981ae64aeb2dff9de979ab82c3490241554cfe
hanfei <1975772822@qq.com>
2017-06-02 15:21:17 +0800

    发发发

---
+++ src/main/java/tk/mybatis/springboot/Test.java
@@ -0,0 +1,15 @@
+package tk.mybatis.springboot;
+
+/**
+ * @Author: Hanfei
+ * @Date: 2017/6/2
+ * @Company:http://www.hbsddz.com
+ * @Project:mybatis-spring-boot
+ * @Class:Test
+ */
+public class Test {
+
+    public void test(){
+        System.out.println("你好你好");
+    }
+}

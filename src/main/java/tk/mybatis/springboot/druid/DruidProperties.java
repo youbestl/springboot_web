4ca6d506f51c80a25c3089f9d83ef372ac37b96c
hanfei <1975772822@qq.com>
2017-06-02 16:45:02 +0800

    发发发

--- src/main/java/tk/mybatis/springboot/druid/DruidProperties.java
+++ src/main/java/tk/mybatis/springboot/druid/DruidProperties.java
@@ -1,6 +1,7 @@
 package tk.mybatis.springboot.druid;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * 只提供了常用的属性，如果有需要，自己添加
